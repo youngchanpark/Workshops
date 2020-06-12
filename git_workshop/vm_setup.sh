e48e1ea14d0c4aac80a5aa4066f566d97dff9176
Young-Chan Park <young.chan.park93@gmail.com>
2020-06-12 16:05:56 +0900

    Added resources for setting up a VM for the git workshop

---
+++ git_workshop/vm_setup.sh
@@ -0,0 +1,19 @@
+
+
+sudo yum install git tmux
+
+sudo chmod 777 /opt
+
+sudo groupadd git
+
+sudo usermod --gid git ycp93
+
+# Make users aaa - zzz
+# Password same as the username
+for char in {a..z}
+do 
+    user_id=$(echo "$char$char$char")
+    sudo useradd --gid git $user_id
+    echo $user_id | sudo passwd --stdin $user_id
+done
+
